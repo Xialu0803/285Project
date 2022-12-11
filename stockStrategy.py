c05dcf0c55dde48f237be20a54138e9a77b5d88b
qinyi <jieliu5@illinois.edu>
2022-12-10 21:55:49 -0800

    change format

--- stockStrategy.py
+++ stockStrategy.py
@@ -29,10 +29,10 @@ def suggest_stocks(amount, strategy_1):
         stock_list1 = stock_options[strategy_1]
         if i == 0:
             perc = 0.36
-            i=i+1
+            i = i+1
         elif i == 1:
             perc = 0.24
-            i=i+1
+            i = i+1
         elif i == 2:
             perc = 0.4
 
@@ -48,8 +48,8 @@ def suggest_stocks(amount, strategy_1):
             temp["symbol"] = symbol
             temp["symbolName"] = stock_name
             temp['prices'] = [stock[0][3], stock[1][3], stock[2][3], stock[3][3], stock[4][3]]
-            temp['investAmount'] = amount*perc
-            temp['stocknumber'] = temp['investAmount']//stock[4][3]
+            temp['investAmount'] = amount * perc
+            temp['stocknumber'] = temp['investAmount'] // stock[4][3]
             stock_info.append(temp)
         except Exception as e:
             print(e)
@@ -82,20 +82,20 @@ if __name__ == "__main__":
     investAmount3 = portfolio[2]["investAmount"]
     stockNum3 = portfolio[2]["stocknumber"]
     print("The stocks chosen for strategy: "+strategy+" is: ")
-    print("Stock Name:"+name1+" invest Amount:"+str(investAmount1)+" purchased number: "+str(stockNum1))
-    print("Stock Name:"+name2+" invest Amount:"+str(investAmount2)+" purchased number: "+str(stockNum2))
-    print("Stock Name:" + name3 + " invest Amount:" + str(investAmount3) + " purchased number: " + str(stockNum3))
-    currentValue1= stockNum1*yf.Ticker(symbol1).info['regularMarketPrice']
+    print("Stock Name: " + name1 + " invest Amount:" + str(investAmount1) + " purchased number: " + str(stockNum1))
+    print("Stock Name: " + name2 + " invest Amount:" + str(investAmount2)+" purchased number: " + str(stockNum2))
+    print("Stock Name: " + name3 + " invest Amount:" + str(investAmount3) + " purchased number: " + str(stockNum3))
+    currentValue1= stockNum1 * yf.Ticker(symbol1).info['regularMarketPrice']
     currentValue2= stockNum2 * yf.Ticker(symbol2).info['regularMarketPrice']
-    currentValue3= stockNum3*yf.Ticker(symbol3).info['regularMarketPrice']
+    currentValue3= stockNum3 * yf.Ticker(symbol3).info['regularMarketPrice']
     print("current Value of the stock chosen: " )
     print("Stock Name:" + name1 + " Current Value Amount: " + str(currentValue1))
     print("Stock Name:" + name2 + " Current Value Amount: " + str(currentValue2))
     print("Stock Name:" + name3 + " Current Value Amount: " + str(currentValue3))
-    print("total Current Value: "+str(currentValue1+currentValue2+currentValue3))
+    print("total Current Value: " + str(currentValue1+currentValue2+currentValue3))
     print("------------------------------------------------------------------------------------------")
     print("Weekly trend of this strategy for the past 5 days")
-    day1Price=portfolio[0]["prices"][0]*stockNum1+portfolio[1]["prices"][0]*stockNum2+portfolio[2]["prices"][0]*stockNum3
+    day1Price = portfolio[0]["prices"][0] * stockNum1 + portfolio[1]["prices"][0] * stockNum2 + portfolio[2]["prices"][0] * stockNum3
     day2Price = portfolio[0]["prices"][1] * stockNum1 + portfolio[1]["prices"][1] * stockNum2 + portfolio[2]["prices"][
         1] * stockNum3
     day3Price = portfolio[0]["prices"][2] * stockNum1 + portfolio[1]["prices"][2] * stockNum2 + portfolio[2]["prices"][
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     day4Price = portfolio[0]["prices"][3] * stockNum1 + portfolio[1]["prices"][3] * stockNum2 + portfolio[2]["prices"][
         3] * stockNum3
     day5Price = currentValue1+currentValue2+currentValue3
-    print("Day 1: "+str(day1Price))
+    print("Day 1: " + str(day1Price))
     print("Day 2: " + str(day2Price))
     print("Day 3: " + str(day3Price))
     print("Day 4: " + str(day4Price))
